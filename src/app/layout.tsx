cc4706d7725a698fb6dcd5c71460477012e8ee03
souhaib <del.souhaub@gmail.com>
2023-07-12 18:21:44 +0000

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import Script from 'next/script'
-import {Head} from "next/document";
 
 const inter = Inter({subsets: ['latin']})
 
